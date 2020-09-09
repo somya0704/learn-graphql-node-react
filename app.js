ef18974069a807aa547ed20441d67aa0a1a9377b
somya07 <somya.singhal0797@gmail.com>
2020-09-09 21:07:35 +0530

    somya|very basic query and mutation operations

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+    type RootQuery{
+      events: [String!]!
+    }
+    type RootMutation{
+      createEvent(name: String): String
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['a', 'b', 'cdcffffdd']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName
+    }
+  },
+  graphiql: true
+}));
+
+app.listen(3001);

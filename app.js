0beb80e062405f5ca08d8b71c19fb42cb9df296f
somya07 <somya.singhal0797@gmail.com>
2020-09-12 18:06:30 +0530

    somya|events query and mutation

--- app.js
+++ app.js
@@ -5,16 +5,35 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
+    type Event{
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput{
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery{
-      events: [String!]!
+      events: [Event!]!
     }
+
     type RootMutation{
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
+
     schema {
       query: RootQuery
       mutation: RootMutation
@@ -22,11 +41,18 @@ app.use('/graphql', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return ['a', 'b', 'cdcffffdd']
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      }
+      events.push(event)
+      return event;
     }
   },
   graphiql: true

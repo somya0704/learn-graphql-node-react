002ae48adbccaa13386a98579d6a4a10c2751ae4
somya07 <somya.singhal0797@gmail.com>
2020-09-16 22:40:41 +0530

    somya|mongodb connect and query/mutation operations

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -41,21 +41,41 @@ app.use('/graphql', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return events;
+      return Event.find()
+      .then(events => {
+        return events.map(event => {
+          return { ...event._doc, _id: event.id };
+        });
+      }).catch(err => {
+        console.log(err);
+        throw err;
+      });
     },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      }
-      events.push(event)
-      return event;
+        date: new Date(args.eventInput.date)
+      })
+      event.save().then(result => {
+        console.log(result);
+        return { ...result._doc, _id: result._doc._id.toString() };
+      }).catch(err => {
+        console.log(err);
+        throw err;
+      });
     }
   },
   graphiql: true
 }));
 
-app.listen(3001);
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${
+  process.env.MONGO_PASSWORD
+  }@cluster0-shard-00-00.5bn4n.mongodb.net:27017,cluster0-shard-00-01.5bn4n.mongodb.net:27017,cluster0-shard-00-02.5bn4n.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-obz5gp-shard-0&authSource=admin&retryWrites=true&w=majority`, {
+    useUnifiedTopology: true, useNewUrlParser: true
+  }).then(() => {
+    app.listen(3001);
+  }).catch(err => {
+    console.log(err)
+  });

f0dcc817cf12c1bd19d7cf6bed679ac633ff4b35
somya07 <somya.singhal0797@gmail.com>
2020-10-08 23:14:22 +0530

    somya|jwt authentication

--- graphql/resolvers/user.js
+++ graphql/resolvers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/user');
 
 module.exports = {
@@ -16,6 +17,30 @@ module.exports = {
       const result = await user_1.save();
       console.log(result);
       return { ...result._doc, password: "null", _id: result.id };
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+
+  login: async ({email, password}) => {
+    try {
+      const login_user = await User.findOne({ email: email });
+      if (!login_user) {
+        throw new Error('User does not exists');
+      }
+      const is_password_equal = await bcrypt.compare(password, login_user.password);
+      if (!is_password_equal){
+        throw new Error('Password is wrong');
+      }
+      const token = jwt.sign(
+        { userId: login_user.id, email: login_user.email },
+        'somesupersecretkey',
+        {
+          expiresIn: '1h'
+        }
+      );
+      return { userId: login_user.id, token: token, tokenExpiration: 1 };
     } catch (err) {
       console.log(err);
       throw err;
